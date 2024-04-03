4ace5186e6b02f06a03ba8f9c43e93dea2d59252
Ambar <ambar.aguilar.c@gmail.com>
2024-04-02 21:09:24 -0300

    Cambios directorio de rutas

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ app.use( express.json());
 //auth => crear usuario, login, renew
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
+
+
+app.get('*', (req, res) => {
+    res.sendFile( __dirname + '/public/index.html');
+})
 //CRUD => eventos
 
 //escuchar peticiones
